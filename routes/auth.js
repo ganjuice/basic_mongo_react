3921ca199574a31599a5af0f9eda385311e23847
Amit Ganju <ganjuamit@gmail.com>
2018-02-05 11:23:07 -0600

    passing to will

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const passport = require('passport')
+const Account = require('../models/account')
+const router = express.Router();
+
+// get for the login
+// a way to display the user
+
+//registering
+router.post("/register", function(req, res){
+	Account.register(new Account({
+		username: req.body.username
+	},
+	req.body.password,
+	function(err, account){
+		if(err){
+			// that didn't work
+			return res.json(false);
+		}
+
+		passport.authenticate('local')(req, res, function(){
+			res.json(true);
+		})
+	}))
+});
+// login
+router.post("/login", passport.authenticate('local'), function(req, res){
+	res.json(true)
+});
+
+router.get('/logout', function(req, res){
+	req.logout();
+	res.json(true)
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 const express = require("express");
 const path = require("path");
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session');
+
 const PORT = process.env.PORT || 3001;
 
+const authRoutes = require('./routes/auth');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+mongoose.connect('mongodb://localhost/basic_mongo_react');
+
+// Initialize our session
+app.use(expressSession({
+	secret: "keyboard cat",
+	resave: false,
+	saveUninitialized: false
+}));
+
+
 
 if(process.env.NODE_ENV ==="production"){
 	app.use(express.static("client/build"));
 }
 
-app.get("/test", function(req, res){
-	res.send("all good!");
+app.use('api/auth', authRoutes);
+
+app.get("/api/test", function(req, res){
+	return res.json("all good!");
 })
 
 app.get("*", function(req, res){
