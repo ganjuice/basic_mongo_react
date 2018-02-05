29de0c683c1077cf3c268626b717488e513569b3
Amit Ganju <ganjuamit@gmail.com>
2018-02-04 18:18:24 -0600

    deploy this please

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
-
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+if(process.env.NODE_ENV ==="production"){
+	app.use(express.static("client/build"));
+}
 
 app.get("/test", function(req, res){
 	res.send("all good!");
 })
 
+app.get("*", function(req, res){
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+
 app.listen(PORT, function(){
-	console.log('Serving on port ${PORT}')
+	console.log('Serving on port ${PORT}');
 })
